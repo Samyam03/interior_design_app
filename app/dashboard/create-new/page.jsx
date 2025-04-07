1f505ef768dacf64229c06e9da97d72f9e2b2188
Samyam03 <samyambhattarai03@gmail.com>
2025-04-06 20:48:26 -1100

    Working with Workspace

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,19 +1,29 @@
+"use client";
+
 import React from 'react'
 import ImageSelection from './_components/ImageSelection'
+import Image from 'next/image'
 
 function CreateNew() {
-  return (
-    <div>
-       <h2 className="font-bold text-3xl text-primary text-center">Use the power of AI for Interior Designing</h2>
-        <p className='text-center text-gray-500 '>Transform you room in a moment using these features.</p>
-    
-    <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-items-center gap-4 mt-10">
-    <ImageSelection />
-    </div>
-     
 
-    </div>
-  )
+    const onHandleInputChange= (value,fieldName) => {
+        // your logic here
+    }
+
+    return (
+        <div>
+            <h2 className="font-bold text-3xl text-primary text-center">
+                Use the power of AI for Interior Designing
+            </h2>
+            <p className='text-center text-gray-500'>
+                Transform your room in a moment using these features.
+            </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-items-center gap-4 mt-10">
+                <ImageSelection selectedImage={(value)=>onHandleInputChange(value,'image')} />
+            </div>
+        </div>
+    )
 }
 
 export default CreateNew
